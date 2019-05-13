1148d0c3c1dc83290b655f01695b5aa17524edad
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-05-14 01:09:31 +0900

    0514

--- learn-sequelize/models/index.js
+++ learn-sequelize/models/index.js
@@ -1,37 +1,16 @@
-'use strict';
+const path = require('path');//path 모듈
+const Sequelize = require('sequelize');//시퀄라이즈 모듈
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize'); //시퀄라이즈 모듈
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';//환경 변수이다. 개발 연습중이기 때문에 development로 설정. 후에 배포용으로 쓸 경우 production을 쓰면 된다. 많은 패키지들의 설정이 바뀐다.(개발용, 배포용)
-const config = require(__dirname + '/../config/config.json')[env];//시퀄라이즈에 대한 설정 파일을 불러오는 코드이다.
-const db = {};
+const env = process.env.NODE_ENV||'development';
+const config = require('../config/config.json')[env];
+//환경 변수이다. 개발용일경우 'development'로, 배포용일경우 'production'으로 쓴다. 패키지의 설정이 이것으로 바뀐다.
+const sequelize = new Sequelize(config.database, config.username,config.password,config);
+//이 시퀄라이즈가 사실 생성자다. 이를 인스턴스화하는 코드이다. 구조는 그냥 외우자.
+const db = {}; //db객체이다.(이 객체를 앞으로 모듈화 해 사용 할 것이다.)
+db.Sequelize = Sequelize;//db객체에 시퀄라이즈 패키지를 넣는다.
+db.Sequelize = sequelize;//db객체에 시퀄라이즈 인스턴스를 넣는다.
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.User = require('./user')(sequelize,Sequelize);
+db.Comment = require('./comment')(sequelize,Sequelize);
 
 module.exports = db;
---
+++ learn-sequelize/models/user.js
@@ -0,0 +1,5 @@
+modules.exports = (sequelize,DataTypes)=>{
+    return sequelize.define('user',{
+
+    });
+}
