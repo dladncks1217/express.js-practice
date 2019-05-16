4eb566b1cdaca24554a978571993133e6ce34577
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-05-16 13:24:21 +0900

    0516

--- learn-sequelize/models/comment.js
+++ learn-sequelize/models/comment.js
@@ -1,5 +1,20 @@
 modules.exports = (sequelize,DataTypes)=>{
     return sequelize.define('user',{
+        commenter:{
 
+        },
+        comment:{
+
+        },
+        created_at:{
+
+        }
+    }, {
+        timestamps:false,
+        underscored:true,
     });
-}
+};
+//comment테이블
+//작성자, 댓글내용, 작성일
+//zero, 안녕하세요, 2019-05-16
+//nero, 으하하하, 2019-05-15
