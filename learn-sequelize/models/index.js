eb5801a732a0be52f28b5463fdd69f72b895fa8f
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-05-17 23:53:41 +0900

    0517

--- learn-sequelize/models/index.js
+++ learn-sequelize/models/index.js
@@ -13,7 +13,7 @@ db.Sequelize = sequelize;//db객체에 시퀄라이즈 인스턴스를 넣는다
 db.User = require('./user')(sequelize,Sequelize);
 db.Comment = require('./comment')(sequelize,Sequelize);
 
-db.User.hasMany(db.Comment);
-db.Comment.belongsTo(db.User);
+db.User.hasMany(db.Comment,{foreignKey:'commenter',sourceKey:'id'});
+db.Comment.belongsTo(db.User,{foreignKey:'commenter', targetKey:'id'});
 
 module.exports = db;
