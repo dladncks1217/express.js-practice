a126f9647041286ec385bd5ef39f589af884c168
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-05-27 16:16:02 +0900

    0527

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var {sequelize} = require('../models/index');
+var {sequelize} = require('./models/index');
 
 var app = express();
 sequelize.sync();
