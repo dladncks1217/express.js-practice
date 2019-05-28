74b9dfa12903b222793de8d9bb3d9b48f9c86a84
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-05-28 13:40:45 +0900

    0528

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -1,14 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const  path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const {sequelize} = require('./models/index');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var {sequelize} = require('./models/index');
-
-var app = express();
+const app = express();
 sequelize.sync();
 
 // view engine setup
