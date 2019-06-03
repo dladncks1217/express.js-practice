09adba4ba2f751049fef8cd0d58e5e8946b7e86c
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-06-03 20:36:00 +0900

    0603

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes');
 const usersRouter = require('./routes/users');
-const commentRouter = require('/routes/comments');
+const commentsRouter = require('/routes/comments');
 const {sequelize} = require('./models');
 
 
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/comments',commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
