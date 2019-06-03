28af41ec702a8d87c0bef80235359788e8f72289
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-06-03 20:45:53 +0900

    0603

--- learn-sequelize/routes/comments.js
+++ learn-sequelize/routes/comments.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/',(req,res,next)=>{
+
+
+});
+router.post('/'(req,res,next)=>{
+
+});
+router.patch('/',(req,res,next)=>{
+
+});
+router.delete('/',(req,res,next)=>{
+
+});
 
 module.exports = router;
