952baa3da9df0f19345905070e25ce56ff4ad998
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-06-05 19:45:50 +0900

    0605

--- learn-sequelize/routes/users.js
+++ learn-sequelize/routes/users.js
@@ -15,18 +15,18 @@ router.get('/', (req, res, next)=>{
 });
 router.post('/',(req,res,next)=>{
     User.create({
-        name:'',
-        age:'',
-        married:'',
+        name:req.body.name,
+        age:req.body.age,
+        married:req.body.married,
     })
     .then((result)=>{
         console.log(result);
         res.status(201).json(result);
     })
     .catch((err)=>{
-        console.error(err);
+     console.error(err);
         next(err);
-    })
+    });
 });
 
 module.exports = router;
