c98bfae3682986a29894c09265cc13532ef32d44
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-06-23 18:02:22 +0900

    0623

--- learn-sequelize/routes/comments.js
+++ learn-sequelize/routes/comments.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {User, Comment} = require('../models');
 //GET/comments/:id
 router.get('/:id',(req,res,next)=>{
     Comment.findAll()
--- learn-sequelize/routes/users.js
+++ learn-sequelize/routes/users.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var {User} = require('../models');
+const express = require('express');
+const router = express.Router();
+const {User} = require('../models');
 
 /* GET users listing. */
 router.get('/', (req, res, next)=>{
