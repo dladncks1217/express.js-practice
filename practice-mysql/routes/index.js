63e07db7249384726ae6874452caabf7a4a53c09
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-08 21:47:17 +0900

    0708

--- practice-mysql/routes/index.js
+++ practice-mysql/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.sendFile('c:\\Users\\dlaxo\\express\\practice-mysql\\views\\index.html');
 });
 
 module.exports = router;
