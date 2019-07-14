1294c3c32b32f7eba07fba1aba974178885b31d3
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-14 15:58:58 +0900

    0714

--- practice-mysql/public/javascripts/example.js
+++ practice-mysql/public/javascripts/example.js
@@ -1,1 +1,9 @@
-return 1;
+let now = new Date();
+let hour = now.getHours();
+let minute = now.getMinuets();
+let second = now.getSeconds();
+
+let func = ()=>{
+    return `${hour}:${minute}:${second}`;
+}
+func();
