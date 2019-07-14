417936ed0a3e671a0ef0ecf6f3331ad862e5644b
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-14 14:36:25 +0900

    0714

--- practice-mysql/app.js
+++ practice-mysql/app.js
@@ -13,6 +13,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname,'public'))); //static파일들
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
