d5011e7001b56b4bfabd2ded66c96e45290aef88
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-26 20:20:04 +0900

    0726

--- practice express-session/app.js
+++ practice express-session/app.js
@@ -1,28 +1,36 @@
-const express = require("express");
-const logger = require('morgan');
-const path = require('path');
-const flash = require('flash');
+const express = require('express');
+const parseurl = require('parseurl');
 const session = require('express-session');
+const sessionstore = require('session-file-store')(session);
 
-const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
-app.use('/',indexRouter);
 
-app.use((req,res,next)=>{
-    const err = new Error('NOT FOUND');
-    err.status =404;
-    next(err);
-});
 
-app.use((err,req,res)=>{
-    res.status(err.status||500);
-    res.render ('error');
+app.use(session({
+  secret: 'asdf',
+  resave: false,    //세션 데이터가 바뀌기 전까지는 세션 저장소의 값을 저장하지 않는다. 만약 true면 값이 바뀌건 바뀌지 않건 계속 저장소에 저장을 한다. 그냥 false로하자.
+  saveUninitialized: true, //세션이 필요하기 전까지 세션을 구동시키지 않는다. 서버에 부담가니까 그냥 true쓰자. 
+  store: new sessionstore(),
+}));
+ 
+
+ 
+app.get('/', function (req, res, next) {
+    console.log(req.session);
+    if(req.session.num === undefined){
+        req.session.num = 1;
+    } else{
+        req.session.num+=1;
+    }
+    res.send(`Views:${req.session.num}`);
+  res.send('hello session');
 });
+ 
+
 
 app.listen(3000,()=>{
-    console.log('3000번 포트에서 서버 대기중입니다.');
+    console.log('3000번포트');
 });
-
-module.exports = app;
