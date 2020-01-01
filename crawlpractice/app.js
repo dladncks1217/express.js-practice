f4dfbfef9a5c7adb4d6035d47ce209ec911e062b
dladncks1217 <dlaxodud1217@gmail.com>
2020-01-01 17:18:38 +0900

    플젝확인용

--- crawlpractice/app.js
+++ crawlpractice/app.js
@@ -1,32 +1,18 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const index = require('./routes/index');
+
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.set('port', process.env.PORT||8800);
+
+app.use('/',index);
+
 /*
-const request = require('request');
-const cheerio = require('cheerio');
-const iconv = require('iconv-lite');
-
-
-
-const reqOption = {
-    method:"GET",
-    url:'http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10004&searchBun=51',
-    headers:{
-       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
-    },
-    encoding:null
-};
-
-request.get(reqOption,(err,res,body)=>{
-    const $ = cheerio.load(body);
-    const $use = $("span#ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle").children('a');
-     iconv.extendNodeEncodings(); 
-    const usecontent = new Buffer($use.toString());
-    console.log(iconv.decode(usecontent,'EUC-KR').toString());
-
-    iconv.extendNodeEncodings();
-    const strContents = new Buffer(body); 
-    console.log(iconv.decode(strContents,'euckr').toString());
-});
- */
-
 const client = require('cheerio-httpcli');
 client.fetch("http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10004&searchBun=51", {}, function (err, $, res, body) {
   const list = $(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle");
@@ -34,4 +20,21 @@ client.fetch("http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10004&searchBun=51
     let result = $(this).find('a').text();
     console.log(result);
   });
+});
+*/
+app.use((error,res,next)=>{
+  const err = new Error('NOT FOUND');
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err,res,next)=>{
+  res.locals.message = err.message;
+    res.locals.message = req.app.get('env') === 'development' ? err :{};
+    res.status(err.status||500);
+    res.render('error');
+});
+
+app.listen(app.get('port'),()=>{
+  console.log(`${app.get('port')}번 포트에서 서버 대기중`);
 });
