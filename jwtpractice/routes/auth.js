529bef0d8b9782f05d5837b851aedee1a9d94f28
dladncks1217 <dlaxodud1217@gmail.com>
2022-08-10 21:40:23 +0900

    add jwt code

--- jwtpractice/routes/auth.js
+++ jwtpractice/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const { User } = require("../models");
+const jwt = require("../utils/jwt-util");
 
 router.post("/join", async (req, res, next) => {
   const { email, nick, password } = req.body;
@@ -22,6 +23,38 @@ router.post("/join", async (req, res, next) => {
   } catch (error) {
     console.error(error);
     return next(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  //... user 로그인 로직
+  const { email, password } = req.body;
+  const exUser = await User.findOne({ where: { email } });
+  if (exUser) {
+    const result = await bcrypt.compare(password, exUser.password);
+    if (result) {
+      // id, pw가 맞다면..
+      // access token과 refresh token을 발급합니다.
+      const accessToken = jwt.sign(exUser.id);
+      const refreshToken = jwt.refresh();
+
+      // 발급한 refresh token을 redis에 key를 user의 id로 하여 저장합니다.
+      //   redisClient.set(user.id, refreshToken);
+
+      res.status(200).send({
+        // client에게 토큰 모두를 반환합니다.
+        ok: true,
+        data: {
+          accessToken,
+          refreshToken,
+        },
+      });
+    }
+  } else {
+    res.status(401).send({
+      ok: false,
+      message: "없는 사용자입니다.",
+    });
   }
 });
 
